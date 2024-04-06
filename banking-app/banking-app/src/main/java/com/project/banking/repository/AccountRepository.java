6992aa6ca06b4a5f4f159051785fe141723af8ce
Alekya Arra <alekyarra07@gmail.com>
2024-04-07 05:18:56 +0530

    created interface

---
+++ banking-app/banking-app/src/main/java/com/project/banking/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.project.banking.repository;
+
+import com.project.banking.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
+
+
---
+++ banking-app/banking-app/src/main/java/com/project/banking/service/AccountService.java
@@ -0,0 +1,8 @@
+package com.project.banking.service;
+
+import com.project.banking.entity.Account;
+
+public interface AccountService {
+	Account createAccount(Account account);
+
+}
