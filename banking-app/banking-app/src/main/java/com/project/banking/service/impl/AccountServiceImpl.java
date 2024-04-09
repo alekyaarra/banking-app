610ff00bcf9a61be01e3ecfa1a72b984e91d92a3
Alekya Arra <alekyarra07@gmail.com>
2024-04-09 11:39:14 +0530

    updated service

--- banking-app/banking-app/src/main/java/com/project/banking/service/impl/AccountServiceImpl.java
+++ banking-app/banking-app/src/main/java/com/project/banking/service/impl/AccountServiceImpl.java
@@ -1,5 +1,20 @@
 package com.project.banking.service.impl;
 
-public class AccountServiceImpl {
+import com.project.banking.service.AccountService;
+import com.project.banking.dto.AccountDto;
+import com.project.banking.repository.AccountRepository;
+import org.springframework.stereotype.Service;
 
+public class AccountServiceImpl implements AccountService {
+	
+	private AccountRepository accountRepository;
+	
+	public AccountServiceImpl( AccountRepository accountRepository) {
+		this.accountRepository = accountRepository;
+	}
+	
+	@Override
+	public  AccountDto createAccount(AccountDto accountDto) {
+		return null;
+	}
 }
